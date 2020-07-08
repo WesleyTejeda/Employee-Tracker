8695a3a690ffd1dc271bbeaf75e8ee7b81f7c061
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-08 17:12:45 -0400

    View table functions now return results from database

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
   
     // Your password
     password: process.env.DB_PASS,
-    database: "auction_db"
+    database: "company_db"
 });
   
 connection.connect(function(err) {
@@ -24,6 +24,18 @@ connection.connect(function(err) {
 });
 
 let choices = ['Add Department','Add Role','Add Employee','View Departments','View Roles','View Employees','Update Employee Roles','Quit'];
+
+let roles = ['Sales Lead', 'Salesperson', 'Lead Engineer','Software Engineer', 'Accountant','Legal Team Lead', 'Lawyer'];
+let departments = [];
+function getDepartments(){
+    connection.query("SELECT * from department",(err, res) =>{
+        if (err){
+            throw err;
+        }
+        console.table(res);
+        init();
+    })
+}
 
 function init(){
     inquirer.prompt({
@@ -35,37 +47,30 @@ function init(){
         if(option.choice == 'Add Department'){
             //Do something
             addDepartment();
-            init();
         }
         if(option.choice == 'Add Role'){
             //Do something
             addRole();
-            init();
         }
         if(option.choice == 'Add Employee'){
             //Do something
             addEmployee();
-            init();
         }
         if(option.choice == 'View Departments'){
             //Do something
             viewDepartments();
-            init();
         }
         if(option.choice == 'View Roles'){
             //Do something
             viewRoles();
-            init();
         }
         if(option.choice == 'View Employees'){
             //Do something
             viewEmployees();
-            init();
         }
         if(option.choice == 'Update Employee Roles'){
             //Do something
             updateEmployeeRoles();
-            init();
         }
         if(option.choice == 'Quit'){
             endConnection();
@@ -82,23 +87,77 @@ function addRole(){
 }
 
 function addEmployee(){
-    console.log("Adding Employee");
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the employee's first name?",
+            name: "first_name"
+        },
+        {
+            type: "input",
+            message: "What is the employee's last name?",
+            name: "last_name"
+        },
+        {
+            type: "list",
+            message: "Choose a role for employee.",
+            name: "role",
+            choices: roles
+        },
+        {
+            type: "list",
+            message: "Who is the manager for the employee?",
+            name: "manager",
+            choices: ['null']
+        }
+    ]).then(response => {
+        connection.query("INSERT INTO employee SET ?",{
+            first_name: response.first_name,
+            last_name: response.last_name,
+            role_id: null,
+            manager_id: null
+        },(err, res) =>{
+            if (err){
+                throw err;
+            }
+            console.table(res);
+            init();
+        })
+    });
 }
 
 function viewDepartments(){
-    console.log("Viewing Departments");
+    connection.query("SELECT * FROM department",(err, res) =>{
+        if (err)
+            throw err;
+        console.table(res);
+        init();
+    });
 }
 
 function viewEmployees(){
-    console.log("Viewing Employess");
+    connection.query("SELECT * FROM employee",(err, res) =>{
+        if (err){
+            throw err;
+        }
+        console.table(res);
+        init();
+    });
 }
 
 function viewRoles(){
-    console.log("Viewing Roles");
+    connection.query("SELECT * FROM role",(err, res) =>{
+        if (err){
+            throw err;
+        }
+        console.table(res);
+        init();
+    });
 }
 
 function updateEmployeeRoles(){
     console.log("Updating Employee Role");
+    init();
 }
 
 //End connection to database

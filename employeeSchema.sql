dc822d27161ea22d9dbe68446d42b3b0b14b99e9
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-09 23:32:07 -0400

    Updated Schema and Seed, included foreign keys

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -23,8 +23,8 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT DEFAULT NULL,
+    manager_id INT DEFAULT 0,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
     PRIMARY KEY (id)
 );

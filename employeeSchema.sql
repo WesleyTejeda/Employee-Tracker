757fb692b7e9cb1102dbfadb47a39ef1a3ad0593
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-12 18:57:57 -0400

    Updated schema

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -23,7 +23,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT DEFAULT 0,
+    manager_id INT,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
     PRIMARY KEY (id)
